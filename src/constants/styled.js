c2e3639378194878d1754e346fd77f311bfb026f
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-02-05 18:56:41 +0300

    add: constants

---
+++ src/constants/styled.js
@@ -0,0 +1,47 @@
+export const MAX_WIDTH = '730px'
+export const BASE_SIZE = '1rem'
+// export const XS_SIZE = parseInt(BASE_SIZE) * 0.5 + 'rem'
+// export const SM_SIZE = parseInt(BASE_SIZE) * 1 + 'rem'
+export const MD_SIZE = parseInt(BASE_SIZE) * 1.2 + 'rem'
+export const LG_SIZE = parseInt(BASE_SIZE) * 1.3 + 'rem'
+export const XL_SIZE = parseInt(BASE_SIZE) * 1.4 + 'rem'
+export const XXL_SIZE = parseInt(BASE_SIZE) * 24 + 'rem'
+
+export const PRIMARY_COLOR = '#202427'
+export const SECONDARY_COLOR = '#C7CCD1'
+export const THIRD_COLOR = '#2578F4'
+export const PRIMARY_DARK_COLOR = '#959595'
+export const SECONDARY_DARK_COLOR = '#5D5FEF'
+export const THIRD_DARK_COLOR = '#EF5DA8'
+export const DANGER_COLOR = '#EC3535'
+// export const WARNING_COLOR = '#e3d464'
+// export const SUCCESS_COLOR = '#55adc3'
+
+export const PRIMARY_GRADIENT =
+	'linear-gradient(303.79deg, #FCC3E0 -53.74%, #D9BEFB -7.23%, #D7BEFC -4.64%, #BCB1FC 40.96%, #A1AEFC 83.05%, #94BDFC 127.47%, #8EC5FC 170.73%), linear-gradient(0deg, #84FAB0 0%, #85F9BC 21.87%, #88F8D7 42.19%, #8BF6F5 60.94%, #8EDDF5 80.73%, #8FD3F4 100%)'
+// export const SECONDARY_GRADIENT = 'linear-gradient(45deg, rgba(69, 84, 177, 0.5) 0%, rgba(167, 131, 226, 0.7) 100%)'
+
+export const PRIMARY_FONT = 'latoRegular'
+export const SECONDARY_FONT = 'latoBold'
+export const THIRD_FONT = 'latoBold'
+
+// export const BORDER_RADIUS_CIRCLE = '50%'
+export const BORDER_RADIUS_OVAL = parseInt(BASE_SIZE) * 2.4 + 'rem'
+export const BORDER_RADIUS_SUPERELLIPSE = parseInt(BASE_SIZE) * 0.8 + 'rem'
+export const THIN_BORDER = parseInt(BASE_SIZE) * 0.1 + 'rem' + ' solid'
+// export const THICK_BORDER = parseInt(BASE_SIZE) * 0.6 + 'rem' + ' solid'
+
+export const DELAY = '0.2s'
+export const LONG_DELAY = '0.4s ease-in-out'
+
+export const BREAKPOINT_XS_MIN = '0px'
+export const BREAKPOINT_XS_MAX = '576px'
+export const BREAKPOINT_SM_MIN = parseInt(BREAKPOINT_XS_MAX) + 1 + 'px' // 577
+export const BREAKPOINT_SM_MAX = '768px'
+export const BREAKPOINT_MD_MIN = parseInt(BREAKPOINT_SM_MAX) + 1 + 'px' // 769
+export const BREAKPOINT_MD_MAX = '992px'
+export const BREAKPOINT_LG_MIN = parseInt(BREAKPOINT_MD_MAX) + 1 + 'px' // 993
+export const BREAKPOINT_LG_MAX = '1200px'
+export const BREAKPOINT_XL_MIN = parseInt(BREAKPOINT_LG_MAX) + 1 + 'px' // 1201
+export const BREAKPOINT_XL_MAX = '1400px'
+export const BREAKPOINT_XXL_MIN = parseInt(BREAKPOINT_XL_MAX) + 1 + 'px' // 1401
