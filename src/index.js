2fb55acfda6dac77d72b2bd5979d8d4d9da80ebf
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-02-08 19:25:14 +0300

    refactor all

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import Apppp from './Apppp'
-import Appp from './Appp'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-console.log()
-
-console.log()
